cb852d210c0ff1b8435db7ec0b771d56719985c6
robotaleh <alex.santos.ete@gmail.com>
2025-02-27 22:34:49 +0100

    Activa modo carrera (5s) y corrige fallos menores

--- source_code/src/control.c
+++ source_code/src/control.c
@@ -62,7 +62,6 @@ static void update_ideal_linear_speed(void) {
   switch (menu_run_get_control_strategy()) {
     case CONTROL_ENCODERS:
       if (ideal_linear_speed < target_linear_speed) {
-        ideal_linear_speed += MIN_ACCEL_MS2 / CONTROL_FREQUENCY_HZ;
         ideal_linear_speed += get_kinematics().linear_accel / CONTROL_FREQUENCY_HZ;
         if (ideal_linear_speed > target_linear_speed) {
           ideal_linear_speed = target_linear_speed;
