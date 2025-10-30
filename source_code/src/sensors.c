946a160df47be4afda906e1cd98ca87a88e78ac2
robotaleh <alex.santos.ete@gmail.com>
2025-10-30 20:52:13 +0100

    Añade control de BiggerFilter a lectura de sensores

--- source_code/src/sensors.c
+++ source_code/src/sensors.c
@@ -295,7 +295,7 @@ void sensors_update_line_position(void) {
     if (sensor_value >= sensores_umb[sensor]) {
       sensores_detectando_sin_filtro++;
     }
-    if (sensor < sensor_ini_linea || sensor > sensor_fin_linea) {
+    if (menu_run_get_filter_mode() == FILTER_BIGGER && (sensor < sensor_ini_linea || sensor > sensor_fin_linea)) {
       sensor_value = LECTURA_MINIMO_SENSORES_LINEA;
     }
 
